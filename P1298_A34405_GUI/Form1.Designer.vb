96030489ee40d8a7e3df11e59529de08dbef18cc
Dan Parker <dparkermit@yahoo.com>
2014-04-29 09:35:35 -0700

    Adding GUI to Version Control
    First Version of GUI.
    It has basic working funcionality

---
+++ P1298_A34405_GUI/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ButtonSetBaudRate = New System.Windows.Forms.Button()
+        Me.TextBoxBaudRate = New System.Windows.Forms.TextBox()
+        Me.ComboBoxComPorts = New System.Windows.Forms.ComboBox()
+        Me.LabelComMsg = New System.Windows.Forms.Label()
+        Me.SerialPortETM = New System.IO.Ports.SerialPort(Me.components)
+        Me.LabelTime = New System.Windows.Forms.Label()
+        Me.LabelMCUVersion = New System.Windows.Forms.Label()
+        Me.LabelGUIVersion = New System.Windows.Forms.Label()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.LabelState = New System.Windows.Forms.Label()
+        Me.Label93 = New System.Windows.Forms.Label()
+        Me.ButtonSetPosition = New System.Windows.Forms.Button()
+        Me.TextBoxPosition = New System.Windows.Forms.TextBox()
+        Me.TextBoxClockWise = New System.Windows.Forms.TextBox()
+        Me.ButtonClockWise = New System.Windows.Forms.Button()
+        Me.TextBoxCounterClockwise = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.LabelTarget = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LabelPosition = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'ButtonSetBaudRate
+        '
+        Me.ButtonSetBaudRate.Location = New System.Drawing.Point(678, 495)
+        Me.ButtonSetBaudRate.Name = "ButtonSetBaudRate"
+        Me.ButtonSetBaudRate.Size = New System.Drawing.Size(114, 23)
+        Me.ButtonSetBaudRate.TabIndex = 845
+        Me.ButtonSetBaudRate.Text = "Set Baud Rate"
+        Me.ButtonSetBaudRate.UseVisualStyleBackColor = True
+        '
+        'TextBoxBaudRate
+        '
+        Me.TextBoxBaudRate.Location = New System.Drawing.Point(678, 469)
+        Me.TextBoxBaudRate.Name = "TextBoxBaudRate"
+        Me.TextBoxBaudRate.Size = New System.Drawing.Size(114, 20)
+        Me.TextBoxBaudRate.TabIndex = 844
+        '
+        'ComboBoxComPorts
+        '
+        Me.ComboBoxComPorts.FormattingEnabled = True
+        Me.ComboBoxComPorts.Location = New System.Drawing.Point(678, 443)
+        Me.ComboBoxComPorts.Name = "ComboBoxComPorts"
+        Me.ComboBoxComPorts.Size = New System.Drawing.Size(114, 21)
+        Me.ComboBoxComPorts.TabIndex = 843
+        '
+        'LabelComMsg
+        '
+        Me.LabelComMsg.AutoSize = True
+        Me.LabelComMsg.Location = New System.Drawing.Point(675, 427)
+        Me.LabelComMsg.Name = "LabelComMsg"
+        Me.LabelComMsg.Size = New System.Drawing.Size(98, 13)
+        Me.LabelComMsg.TabIndex = 842
+        Me.LabelComMsg.Text = "Selected COM Port"
+        '
+        'LabelTime
+        '
+        Me.LabelTime.AutoSize = True
+        Me.LabelTime.Location = New System.Drawing.Point(14, 476)
+        Me.LabelTime.Name = "LabelTime"
+        Me.LabelTime.Size = New System.Drawing.Size(30, 13)
+        Me.LabelTime.TabIndex = 867
+        Me.LabelTime.Text = "Time"
+        '
+        'LabelMCUVersion
+        '
+        Me.LabelMCUVersion.AutoSize = True
+        Me.LabelMCUVersion.Location = New System.Drawing.Point(14, 489)
+        Me.LabelMCUVersion.Name = "LabelMCUVersion"
+        Me.LabelMCUVersion.Size = New System.Drawing.Size(63, 13)
+        Me.LabelMCUVersion.TabIndex = 866
+        Me.LabelMCUVersion.Text = "McuVersion"
+        '
+        'LabelGUIVersion
+        '
+        Me.LabelGUIVersion.AutoSize = True
+        Me.LabelGUIVersion.Location = New System.Drawing.Point(14, 502)
+        Me.LabelGUIVersion.Name = "LabelGUIVersion"
+        Me.LabelGUIVersion.Size = New System.Drawing.Size(64, 13)
+        Me.LabelGUIVersion.TabIndex = 865
+        Me.LabelGUIVersion.Text = "GUI Version"
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(17, 27)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(114, 23)
+        Me.ButtonUpdate.TabIndex = 873
+        Me.ButtonUpdate.Text = "Update Data"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'LabelState
+        '
+        Me.LabelState.AutoSize = True
+        Me.LabelState.Location = New System.Drawing.Point(222, 32)
+        Me.LabelState.Name = "LabelState"
+        Me.LabelState.Size = New System.Drawing.Size(10, 13)
+        Me.LabelState.TabIndex = 875
+        Me.LabelState.Text = "-"
+        '
+        'Label93
+        '
+        Me.Label93.AutoSize = True
+        Me.Label93.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label93.Location = New System.Drawing.Point(171, 32)
+        Me.Label93.Name = "Label93"
+        Me.Label93.Size = New System.Drawing.Size(37, 13)
+        Me.Label93.TabIndex = 874
+        Me.Label93.Text = "State"
+        '
+        'ButtonSetPosition
+        '
+        Me.ButtonSetPosition.Location = New System.Drawing.Point(156, 148)
+        Me.ButtonSetPosition.Name = "ButtonSetPosition"
+        Me.ButtonSetPosition.Size = New System.Drawing.Size(134, 23)
+        Me.ButtonSetPosition.TabIndex = 876
+        Me.ButtonSetPosition.Text = "Set Position"
+        Me.ButtonSetPosition.UseVisualStyleBackColor = True
+        '
+        'TextBoxPosition
+        '
+        Me.TextBoxPosition.Location = New System.Drawing.Point(17, 151)
+        Me.TextBoxPosition.Name = "TextBoxPosition"
+        Me.TextBoxPosition.Size = New System.Drawing.Size(114, 20)
+        Me.TextBoxPosition.TabIndex = 877
+        '
+        'TextBoxClockWise
+        '
+        Me.TextBoxClockWise.Location = New System.Drawing.Point(17, 177)
+        Me.TextBoxClockWise.Name = "TextBoxClockWise"
+        Me.TextBoxClockWise.Size = New System.Drawing.Size(114, 20)
+        Me.TextBoxClockWise.TabIndex = 879
+        '
+        'ButtonClockWise
+        '
+        Me.ButtonClockWise.Location = New System.Drawing.Point(156, 174)
+        Me.ButtonClockWise.Name = "ButtonClockWise"
+        Me.ButtonClockWise.Size = New System.Drawing.Size(134, 23)
+        Me.ButtonClockWise.TabIndex = 878
+        Me.ButtonClockWise.Text = "Move Clockwise"
+        Me.ButtonClockWise.UseVisualStyleBackColor = True
+        '
+        'TextBoxCounterClockwise
+        '
+        Me.TextBoxCounterClockwise.Location = New System.Drawing.Point(17, 203)
+        Me.TextBoxCounterClockwise.Name = "TextBoxCounterClockwise"
+        Me.TextBoxCounterClockwise.Size = New System.Drawing.Size(114, 20)
+        Me.TextBoxCounterClockwise.TabIndex = 881
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(156, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(134, 23)
+        Me.Button2.TabIndex = 880
+        Me.Button2.Text = "Move Counter Clockwise"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'LabelTarget
+        '
+        Me.LabelTarget.AutoSize = True
+        Me.LabelTarget.Location = New System.Drawing.Point(222, 57)
+        Me.LabelTarget.Name = "LabelTarget"
+        Me.LabelTarget.Size = New System.Drawing.Size(38, 13)
+        Me.LabelTarget.TabIndex = 882
+        Me.LabelTarget.Text = "Target"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(171, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 883
+        Me.Label2.Text = "Target"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(171, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 885
+        Me.Label1.Text = "Position"
+        '
+        'LabelPosition
+        '
+        Me.LabelPosition.AutoSize = True
+        Me.LabelPosition.Location = New System.Drawing.Point(222, 81)
+        Me.LabelPosition.Name = "LabelPosition"
+        Me.LabelPosition.Size = New System.Drawing.Size(44, 13)
+        Me.LabelPosition.TabIndex = 884
+        Me.LabelPosition.Text = "Position"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(820, 530)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LabelPosition)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.LabelTarget)
+        Me.Controls.Add(Me.TextBoxCounterClockwise)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBoxClockWise)
+        Me.Controls.Add(Me.ButtonClockWise)
+        Me.Controls.Add(Me.TextBoxPosition)
+        Me.Controls.Add(Me.ButtonSetPosition)
+        Me.Controls.Add(Me.LabelState)
+        Me.Controls.Add(Me.Label93)
+        Me.Controls.Add(Me.ButtonUpdate)
+        Me.Controls.Add(Me.LabelTime)
+        Me.Controls.Add(Me.LabelMCUVersion)
+        Me.Controls.Add(Me.LabelGUIVersion)
+        Me.Controls.Add(Me.ButtonSetBaudRate)
+        Me.Controls.Add(Me.TextBoxBaudRate)
+        Me.Controls.Add(Me.ComboBoxComPorts)
+        Me.Controls.Add(Me.LabelComMsg)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonSetBaudRate As System.Windows.Forms.Button
+    Friend WithEvents TextBoxBaudRate As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBoxComPorts As System.Windows.Forms.ComboBox
+    Friend WithEvents LabelComMsg As System.Windows.Forms.Label
+    Friend WithEvents SerialPortETM As System.IO.Ports.SerialPort
+    Friend WithEvents LabelTime As System.Windows.Forms.Label
+    Friend WithEvents LabelMCUVersion As System.Windows.Forms.Label
+    Friend WithEvents LabelGUIVersion As System.Windows.Forms.Label
+    Friend WithEvents ButtonUpdate As System.Windows.Forms.Button
+    Friend WithEvents LabelState As System.Windows.Forms.Label
+    Friend WithEvents Label93 As System.Windows.Forms.Label
+    Friend WithEvents ButtonSetPosition As System.Windows.Forms.Button
+    Friend WithEvents TextBoxPosition As System.Windows.Forms.TextBox
+    Friend WithEvents TextBoxClockWise As System.Windows.Forms.TextBox
+    Friend WithEvents ButtonClockWise As System.Windows.Forms.Button
+    Friend WithEvents TextBoxCounterClockwise As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents LabelTarget As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LabelPosition As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+End Class
---
+++ P1298_A34405_GUI/Form1.vb
@@ -0,0 +1,407 @@
+﻿'Imports System
+'Imports System.IO
+'Imports System.Collections
+
+Public Class Form1
+    Dim autostep As UInt16
+    Dim autointerval As Integer
+    Dim power_ramp As UInt16
+    Dim power_ramp_dan_state As Integer
+
+    Public ReturnData As UInt16
+    Dim ComError As Boolean
+    Dim Tic As Boolean = False
+    Dim SerialCommandTransmitBuffer(9) As Byte
+    Dim SerialCommandReceiveBuffer(9) As Byte
+    Public Const SYNC_1 As Byte = &HF1
+    Public Const SYNC_2 As Byte = &HF2
+    Public Const SYNC_3_SEND As Byte = &HF3
+    Public Const SYNC_3_RECIEVE As Byte = &HF4
+    Public Const COMMAND_LENGTH As Byte = 9
+    Dim fileName As String
+    Dim filePath As String
+    Dim file As System.IO.StreamWriter
+
+
+
+
+    ' Command List
+    Public Const CMD_READ_RAM_VALUE As Byte = &H20
+    Public Const CMD_SET_TARGET_POSITION As Byte = &H30
+    Public Const CMD_MOVE_CLOCKWISE As Byte = &H32
+    Public Const CMD_MOVE_COUNTER_CLOCKWISE As Byte = &H34
+    Public Const CMD_READ_EEPROM_REGISTER As Byte = &H40
+    Public Const CMD_WRITE_EEPROM_REGISTER As Byte = &H42
+
+
+
+
+
+
+    'RAM Locations
+    Public Const RAM_READ_STATE As Byte = &H1
+    Public Const RAM_READ_VERSION As Byte = &H2
+
+    Public Const RAM_READ_CURRENT_POSITION As Byte = &H10
+    Public Const RAM_READ_TARGET_POSITION As Byte = &H20
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBoxComPorts.Items.Add(sp)
+        Next
+
+        TextBoxBaudRate.Text = My.Settings.SelectedBaudRate
+
+        Try
+            ComboBoxComPorts.SelectedText = My.Settings.SelectedComPortString
+        Catch ex As Exception
+            MsgBox("Please select a Valid Com Port")
+        End Try
+
+        If (System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed) Then
+            Dim verDeployed As System.Version = My.Application.Deployment.CurrentVersion
+            LabelGUIVersion.Text = "GUI Version " & verDeployed.ToString
+        Else
+            LabelGUIVersion.Text = "GUI Version not Available in Debug Mode"
+        End If
+
+        LabelMCUVersion.Text = "PIC Version "
+
+        OpenLogFile()
+    End Sub
+
+
+    Private Sub OpenSerialPortETM()
+        Try
+            If SerialPortETM.IsOpen Then
+                SerialPortETM.Close()
+            End If
+        Catch ex As Exception
+            LabelComMsg.Text = "Serial Port Open/Close Error"
+            ComError = True
+        End Try
+
+        Try
+            SerialPortETM.PortName = My.Settings.SelectedComPortString
+            SerialPortETM.ReadTimeout = 25
+            SerialPortETM.BaudRate = My.Settings.SelectedBaudRate
+
+        Catch ex As Exception
+            MsgBox("Please Select a valid Com Port")
+        End Try
+
+        Try
+            SerialPortETM.Open()
+        Catch ex As Exception
+            LabelComMsg.Text = "Serial Port Failed to Open"
+            ComError = True
+        End Try
+    End Sub
+
+    Private Sub CloseSerialPortETM()
+        Try
+            SerialPortETM.Close()
+        Catch ex As Exception
+            LabelComMsg.Text = "Serial Port Failed to Close"
+            ComError = True
+        End Try
+    End Sub
+
+    Private Function CheckCRC() As Boolean
+        Dim CrcCheck As UInt16 = 0
+        Dim CRC As UInt16 = 0
+        CrcCheck = Int(SerialCommandReceiveBuffer(0))
+        CrcCheck = CrcCheck + Int(SerialCommandReceiveBuffer(1))
+        CrcCheck = CrcCheck + Int(SerialCommandReceiveBuffer(2))
+        CrcCheck = CrcCheck + Int(SerialCommandReceiveBuffer(3))
+        CrcCheck = CrcCheck + Int(SerialCommandReceiveBuffer(4))
+        CrcCheck = CrcCheck + Int(SerialCommandReceiveBuffer(5))
+        CrcCheck = CrcCheck + Int(SerialCommandReceiveBuffer(6))
+        CRC = Int(SerialCommandReceiveBuffer(7)) * 256 + Int(SerialCommandReceiveBuffer(8))
+        If CrcCheck = CRC Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Function SendAndValidateCommand(ByVal commandB As Byte, ByVal locationB As Byte, ByVal DataHB As Byte, ByVal DataLB As Byte) As Boolean
+        Dim valid_command As Boolean = False
+        Dim CheckSum As UInt16 = 0
+
+
+        SerialCommandTransmitBuffer(0) = SYNC_1
+        SerialCommandTransmitBuffer(1) = SYNC_2
+        SerialCommandTransmitBuffer(2) = SYNC_3_SEND
+        SerialCommandTransmitBuffer(3) = commandB
+        SerialCommandTransmitBuffer(4) = DataHB
+        SerialCommandTransmitBuffer(5) = DataLB
+        SerialCommandTransmitBuffer(6) = locationB
+
+
+
+        CheckSum = SYNC_1
+        CheckSum = CheckSum + SYNC_2
+        CheckSum = CheckSum + SYNC_3_SEND + commandB + DataHB + DataLB + locationB
+
+        SerialCommandTransmitBuffer(7) = Int(CheckSum / 256)
+        SerialCommandTransmitBuffer(8) = CheckSum Mod 256
+
+
+        Try
+            If SerialPortETM.IsOpen = False Then
+                OpenSerialPortETM()
+            End If
+            SerialPortETM.Write(SerialCommandTransmitBuffer, 0, 9)
+        Catch ex As Exception
+            LabelComMsg.Text = "Serial Port Failed to Write Command Buffer"
+            Return False
+        End Try
+
+        Try
+            SerialCommandReceiveBuffer(0) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(1) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(2) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(3) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(4) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(5) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(6) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(7) = SerialPortETM.ReadByte
+            SerialCommandReceiveBuffer(8) = SerialPortETM.ReadByte
+
+        Catch ex As Exception
+            LabelComMsg.Text = "Serial Port Failed to Read Return"
+            Return False
+        End Try
+
+
+
+        Try
+
+            If SerialCommandReceiveBuffer(0) = SYNC_1 Then
+                If SerialCommandReceiveBuffer(1) = SYNC_2 Then
+                    If SerialCommandReceiveBuffer(2) = SYNC_3_RECIEVE Then
+                        If CheckCRC() = True Then
+                            valid_command = True
+                            LabelComMsg.Text = "Message Sent/Received"
+                            'A valid return was recieved.  Check what the responce was
+                            If SerialCommandReceiveBuffer(3) <> SerialCommandTransmitBuffer(3) Then
+                                'The pic was not able to process the command
+                                If SerialCommandReceiveBuffer(3) = &H1 Then
+                                    LabelComMsg.Text = "ETM Controller Responds - Unknown Command"
+                                ElseIf SerialCommandReceiveBuffer(3) = &H22 Then
+                                    LabelComMsg.Text = "ETM Controller Responds - Program Value out of Valid Range"
+                                ElseIf SerialCommandReceiveBuffer(3) = &H23 Then
+                                    LabelComMsg.Text = "ETM Controller Responds - EEPROM ERROR - Unable to set value"
+                                Else
+                                    LabelComMsg.Text = "ETM Controller Responce Error - " & SerialCommandReceiveBuffer(3)
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            End If
+            If valid_command <> True Then
+                LabelComMsg.Text = "Serial Port return data was not valid"
+                SerialPortETM.Close()
+            End If
+        Catch ex As Exception
+            LabelComMsg.Text = "Error processing Serial Port data"
+            ComError = True
+            Return 0
+        End Try
+
+        ReturnData = SerialCommandReceiveBuffer(4) * 256 + SerialCommandReceiveBuffer(5)
+        Return valid_command
+    End Function
+
+
+    Private Sub ReadAllFromRam()
+        LabelTime.Text = DateTime.Now
+
+        'Read Control State
+        If SendAndValidateCommand(CMD_READ_RAM_VALUE, RAM_READ_STATE, 0, 0) = True Then
+            If ReturnData = &H10 Then
+                LabelState.Text = "Start Up"
+            ElseIf ReturnData = &H20 Then
+                LabelState.Text = "Motor Zero"
+            ElseIf ReturnData = &H24 Then
+                LabelState.Text = "Motor Startup Home"
+            ElseIf ReturnData = &H30 Then
+                LabelState.Text = "Reset"
+            ElseIf ReturnData = &H40 Then
+                LabelState.Text = "AFC - Not Pulsing"
+            ElseIf ReturnData = &H44 Then
+                LabelState.Text = "AFC - Start Up"
+            ElseIf ReturnData = &H48 Then
+                LabelState.Text = "AFC - Steady State"
+            ElseIf ReturnData = &H50 Then
+                LabelState.Text = "Manual Mode"
+            ElseIf ReturnData = &H60 Then
+                LabelState.Text = "Serial Control Mode"
+            ElseIf ReturnData = &HF0 Then
+                LabelState.Text = "Fault"
+            End If
+        Else
+            LabelState.Text = "error"
+            Exit Sub
+        End If
+
+        ' Read Pic Version
+        If SendAndValidateCommand(CMD_READ_RAM_VALUE, RAM_READ_VERSION, 0, 0) = True Then
+            LabelMCUVersion.Text = ReturnData
+        Else
+            LabelMCUVersion.Text = "error"
+            Exit Sub
+        End If
+
+        ' Read Current Position
+        If SendAndValidateCommand(CMD_READ_RAM_VALUE, RAM_READ_CURRENT_POSITION, 0, 0) = True Then
+            LabelPosition.Text = ReturnData
+        Else
+            LabelPosition.Text = "error"
+            Exit Sub
+        End If
+
+
+        ' Read Target Position
+        If SendAndValidateCommand(CMD_READ_RAM_VALUE, RAM_READ_TARGET_POSITION, 0, 0) = True Then
+            LabelTarget.Text = ReturnData
+        Else
+            LabelTarget.Text = "error"
+            Exit Sub
+        End If
+
+
+
+    End Sub
+
+
+
+
+    Private Sub OpenLogFile()
+        fileName = "ETM_AFC_Log_" & DateTime.Now.ToString("yyyy_MM_dd_HH_mm") & ".csv"
+        filePath = System.IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.MyDocuments, fileName)
+        file = My.Computer.FileSystem.OpenTextFileWriter(filePath, True)
+        file.Write("Time , ")
+        file.Write("State , ")
+        'file.Write("Target Power , ")
+        'file.Write("Fwd A Power , ")
+        'file.Write("Fwd B Power, ")
+        'file.Write("Pid Dac Out , ")
+        'file.Write("RF Amp Temp , ")
+        'file.WriteLine("")
+        file.Close()
+    End Sub
+
+    Private Sub WriteToLogFile()
+        file = My.Computer.FileSystem.OpenTextFileWriter(filePath, True)
+        file.Write(LabelTime.Text & " , ")
+        file.Write(LabelState.Text & " , ")
+        'file.Write(LabelTargetPower.Text & " , ")
+        'file.Write(LabelFwdAPower.Text & " , ")
+        'file.Write(LabelFwdBPower.Text & " , ")
+        'file.Write(LabelDacOut.Text & " , ")
+        'file.Write(LabelRFAmpTemp.Text & " , ")
+        file.WriteLine("")
+        file.Close()
+    End Sub
+
+
+    Private Sub ButtonSetBaudRate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonSetBaudRate.Click
+        Try
+            My.Settings.SelectedBaudRate = TextBoxBaudRate.Text
+        Catch ex As Exception
+            MsgBox("Baud Rate not a valid Number")
+        End Try
+
+
+        Try
+            SerialPortETM.BaudRate = My.Settings.SelectedBaudRate
+        Catch ex As Exception
+        End Try
+    End Sub
+
+
+    Private Sub ComboBoxComPorts_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBoxComPorts.SelectedIndexChanged
+        Try
+            My.Settings.SelectedComPortString = ComboBoxComPorts.SelectedItem.ToString
+
+        Catch ex As Exception
+            MsgBox("Please select a Valid Com Port")
+        End Try
+        Try
+            OpenSerialPortETM()
+        Catch ex As Exception
+            MsgBox("Unable to open selected Com Port")
+        End Try
+    End Sub
+
+    Private Sub ButtonUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonUpdate.Click
+        ReadAllFromRam()
+    End Sub
+
+    Private Sub ButtonSetPosition_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonSetPosition.Click
+        Dim ProgramWord As UInt16
+        Dim ProgramHB As Byte
+        Dim ProgramLB As Byte
+
+        Try
+            ProgramWord = Int(TextBoxPosition.Text)
+            ProgramHB = Int(ProgramWord / 256)
+            ProgramLB = ProgramWord Mod 256
+        Catch ex As Exception
+            MsgBox("Data not Valid")
+        End Try
+
+        If SendAndValidateCommand(CMD_SET_TARGET_POSITION, 0, ProgramHB, ProgramLB) = True Then
+        Else
+            MsgBox("Unable to Set Position")
+        End If
+    End Sub
+
+    Private Sub ButtonClockWise_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonClockWise.Click
+        Dim ProgramWord As UInt16
+        Dim ProgramHB As Byte
+        Dim ProgramLB As Byte
+
+        Try
+            ProgramWord = Int(TextBoxClockWise.Text)
+            ProgramHB = Int(ProgramWord / 256)
+            ProgramLB = ProgramWord Mod 256
+        Catch ex As Exception
+            MsgBox("Data not Valid")
+        End Try
+
+        If SendAndValidateCommand(CMD_MOVE_CLOCKWISE, 0, ProgramHB, ProgramLB) = True Then
+        Else
+            MsgBox("Unable to Move Clockwise")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim ProgramWord As UInt16
+        Dim ProgramHB As Byte
+        Dim ProgramLB As Byte
+
+        Try
+            ProgramWord = Int(TextBoxCounterClockwise.Text)
+            ProgramHB = Int(ProgramWord / 256)
+            ProgramLB = ProgramWord Mod 256
+        Catch ex As Exception
+            MsgBox("Data not Valid")
+        End Try
+
+        If SendAndValidateCommand(CMD_MOVE_COUNTER_CLOCKWISE, 0, ProgramHB, ProgramLB) = True Then
+        Else
+            MsgBox("Unable to Move Clockwise")
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        ReadAllFromRam()
+    End Sub
+End Class
